8c2f76089b1a541589fd4339b877e68b5b5ad0bf
K <JuanEscobarGT@outlook.com>
2024-06-24 23:44:04 -0600

    [🚧backup] Avances para la gestión de hojas de responsabilidades

--- app/Livewire/Backend/Responsability/ResponsabilitySheetCreate.php
+++ app/Livewire/Backend/Responsability/ResponsabilitySheetCreate.php
@@ -8,11 +8,14 @@ class ResponsabilitySheetCreate extends Component
 {
     public $step = 1;
 
-    public $test_val;
-
     public function render()
     {
         return view('livewire.backend.responsability.responsability-sheet-create');
+    }
+
+    public function nextStep()
+    {
+        $this->step++;
     }
 
     public function save()
--- resources/views/livewire/backend/responsability/responsability-sheet-create.blade.php
+++ resources/views/livewire/backend/responsability/responsability-sheet-create.blade.php
@@ -16,15 +16,21 @@
         }
     </style>
 
-    <x-mary-steps wire:model="step" shadow class="my-5 p-5 bg-slate-100 dark:bg-slate-800 rounded-lg">
+    <x-mary-steps wire:model="step" shadow class="my-5 p-5 bg-base-100 rounded-lg">
         <x-mary-step step="1" text="{{ __('General Data') }}">
-            Register step
+            <section>
+                <h2 class="block text-center text-2xl dark:text-white">{{ __('General Data') }}</h2>
+            </section>
         </x-mary-step>
         <x-mary-step step="2" text="{{ __('Assign Items') }}">
-            <x-mary-input wire:model="test_val" label="CAP"></x-mary-input>
+            <section>
+                <h2>{{ __('Assign Items') }}</h2>
+            </section>
         </x-mary-step>
         <x-mary-step step="3" data-content="✓" text="{{ __('Verify Responsibilities') }}">
-            Receive Product
+            <section>
+                <h2>{{ __('Verify Responsibilities') }}</h2>
+            </section>
         </x-mary-step>
     </x-mary-steps>
 
@@ -34,9 +40,8 @@
 
         {{-- Next or Save Button --}}
         <x-btn-save x-show="$wire.step === 3" wire:click="save" spinner />
-        <x-mary-button x-show="$wire.step !== 3" label="{{ __('Next') }}"
-            x-on:click="if ($wire.step < 3) $wire.step = $wire.step + 1" icon="m-arrow-uturn-right"
-            class="btn-primary text-white" />
+        <x-mary-button x-show="$wire.step !== 3" label="{{ __('Next') }}" wire:click='nextStep' spinner
+            icon="m-arrow-uturn-right" class="btn-primary text-white" />
     </div>
 
 </section>
